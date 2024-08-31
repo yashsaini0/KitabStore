87ded20de96e3ea369723e232f31df87416daaf5
yash <yash03saini@gmail.com>
2024-08-31 21:05:32 +0530

    Fix module resolution issues2

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Banner from '../components/Banner'
-import Freebook from '../components/Freebook'
-import Footer from '../components/Footer'
+import Navbar from '../components/Navbar.jsx'
+import Banner from '../components/Banner.jsx'
+import Freebook from '../components/Freebook.jsx'
+import Footer from '../components/Footer.jsx'
 
 function Home() {
   return (
